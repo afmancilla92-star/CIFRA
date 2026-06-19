d855fcb5032c428646954db5dd6f5a65605d530d
afmancilla92-star <afmancilla.92@gmail.com>
2026-06-19 16:28:50 -0500

    Update robots.ts

--- cifra-app/src/app/robots.ts
+++ cifra-app/src/app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/", disallow: "/test" }],
-    sitemap: "https://cifra.example.com/sitemap.xml",
+    sitemap: "https://cifra-mu.vercel.app/sitemap.xml",
   };
 }
